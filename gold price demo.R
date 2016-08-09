9359ed90df6d1fd816ab245e69b86ca8527f7ec9
kevin608051 <kevin608051@hotmail.com>
2016-08-10 02:16:54 +0800

    gold
    gold

--- gold price demo.R
+++ gold price demo.R
@@ -6,9 +6,6 @@ library(httr)
 
 orgURL = 'http://www.gck99.com.tw/gold.php'
 
-alldata = data.frame()
-
-
 html = getURL(orgURL, ssl.verifypeer = FALSE)
 xml = htmlParse(html, encoding ='utf-8')
 address = xpathSApply(xml, "//tr[@class='main_1']//td[1]", xmlValue)
